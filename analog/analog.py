bf084047059b4b665d79d524e798dab2b1202bc3
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-10-09 16:11:00 -0400

    move device placement and hash to StorageHandler

--- analog/analog.py
+++ analog/analog.py
@@ -2,16 +2,18 @@ from functools import partial
 
 from analog.storage import StorageHandler
 from analog.covariance import CovarianceHandler
-from analog.hash import SHA256Hasher
 
 
 class AnaLog:
-    def __init__(self, project, config=None, covariance_handler=None, data_hasher=None):
+    def __init__(
+        self, project, config=None, covariance_handler=None, storage_handler=None
+    ):
         self.project = project
         self.config = config
 
         # hook
         self.modules_to_hook = []
+        self.modules_to_name = {}
         self.forward_hooks = []
         self.backward_hooks = []
         self.tensor_hooks = []
@@ -19,7 +21,6 @@ class AnaLog:
         # handler
         self.storage_handler = StorageHandler(config)
         self.covariance_handler = covariance_handler or CovarianceHandler(config)
-        self.data_hasher = data_hasher or SHA256Hasher(config)
 
         # log
         self.log = None
@@ -31,22 +32,17 @@ class AnaLog:
         self.data_analyzer = None
         self.weight_analyzer = None
 
-    def _hash_tensor(self, tensor):
-        """Computes a hash for a tensor using the provided data hasher."""
-        return self.data_hasher.hash(tensor)
-
     def _forward_hook_fn(self, module, inputs, module_name):
+        """forward hook for module"""
         if self.hessian is not None:
             covariance = self.covariance_handler.compute_covariance(module, inputs)
             self.storage_handler.update_covariance(module_name, "forward", covariance)
 
-        # offload
-        inputs = inputs.cpu()
-
         if self.save and "activations" in self.log:
-            self.storage_handler.push(self.current_data_hash, module, "forward", inputs)
+            self.storage_handler.push(self.current_data_id, module, "forward", inputs)
 
     def _backward_hook_fn(self, module, grad_inputs, grad_outputs, module_name):
+        """backward hook for module"""
         del grad_inputs
 
         if self.hessian is not None:
@@ -55,34 +51,44 @@ class AnaLog:
             )
             self.storage_handler.update_covariance(module_name, "backward", covariance)
 
-        # offload
-        grad_outputs = grad_outputs.cpu()
-
         if self.save and self.log == "full_activations":
             self.storage_handler.push(
-                self.current_data_hash, module, "backward", grad_outputs
+                self.current_data_id, module, "backward", grad_outputs
             )
         elif self.save and self.log == "gradient":
             raise NotImplementedError
 
-    def _tensor_hook_fn(self, grad, tensor_name):
+    def _tensor_forward_hook_fn(self, tensor, tensor_name):
+        """Create forward tensor hook. It's not a real hook, but the name is kept for consistency."""
+        if self.hessian is not None:
+            covariance = self.covariance_handler.compute_covariance(None, tensor)
+            self.storage_handler.update_covariance(tensor_name, "forward", covariance)
+
+        if self.save and "activations" in self.log:
+            self.storage_handler.push(
+                self.current_data_id, tensor_name, "forward", tensor
+            )
+
+    def _tensor_backward_hook_fn(self, grad, tensor_name):
+        """Create backward tensor hook"""
         if self.hessian is not None:
             covariance = self.covariance_handler.compute_covariance(None, grad)
             self.storage_handler.update_covariance(tensor_name, "backward", covariance)
 
-        # offload
-        grad = grad.cpu()
-
         if self.save and self.log == "full_activations":
             self.storage_handler.push(
-                self.current_data_hash, tensor_name, "backward", grad
+                self.current_data_id, tensor_name, "backward", grad
             )
 
     def watch(self, model, type_filter=None, name_filter=None):
-        """Sets up the model to be watched."""
+        """Sets up modules in model to be watched."""
         self.model = model
 
         for name, module in self.model.named_modules():
+            # only consider the leaf module
+            if len(list(module.children())) > 0:
+                continue
+
             if type_filter is not None and not any(
                 isinstance(module, module_type) for module_type in type_filter
             ):
@@ -92,47 +98,40 @@ class AnaLog:
             ):
                 continue
             self.modules_to_hook.append(module)
+            self.modules_to_name[module] = name
 
     def watch_activation(self, tensor_dict):
         """Sets up the tensor to be watched."""
         for name, tensor in tensor_dict.items():
+            self._tensor_forward_hook_fn(tensor, name)
             self.tensor_hooks.append(
-                tensor.register_hook(partial(self._tensor_hook_fn, tensor_name=name))
+                tensor.register_hook(
+                    partial(self._tensor_backward_hook_fn, tensor_name=name)
+                )
             )
 
-            if self.hessian is not None:
-                covariance = self.covariance_handler.compute_covariance(
-                    None, tensor_dict
-                )
-                self.storage_handler.update_covariance(name, "forward", covariance)
-
-            # offload
-            tensor = tensor.cpu()
-
-            if self.save and self.log == "full_activations":
-                self.storage_handler.push(
-                    self.current_data_hash, name, "forward", tensor
-                )
-
     def get_log(self):
-        raise self.current_log
+        return self.current_log
+
+    def get_module_name(self, module):
+        return self.modules_to_name[module]
 
     def finalize(self):
         raise NotImplementedError
 
     def __enter__(
         self,
-        data=None,
+        data_id=None,
         log=None,
         hessian=None,
         save=None,
         test=None,
     ):
         """Sets up the context manager."""
-        self.sanity_check(data, log, hessian, save)
+        self.sanity_check(data_id, log, hessian, save)
 
         self.current_log = None
-        self.current_data_hash = self._hash_tensor(data)
+        self.current_data_id = data_id
 
         self.log = log
         self.hessian = hessian
@@ -173,6 +172,7 @@ class AnaLog:
     def clear(self):
         self.clear_for_exit()
         self.modules_to_hook = []
+        self.modules_to_name = {}
 
     def sanity_check(self, data, log, hessian, save):
         if save and data is None:
