15f1554b5fe55e2be3cf0a6b42755ebcbfde2d61
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-10-16 16:20:46 -0400

    prepare experiments

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+python_requires = ">=3.6.0,<3.11.0"
+
+if __name__ == "__main__":
+    setup(
+        name="analog",
+        version="0.0.0",
+        packages=find_packages(
+            exclude=[
+                "examples",
+                "tests",
+            ]
+        ),
+        install_requires=requirements,
+        python_requires=python_requires,
+    )
