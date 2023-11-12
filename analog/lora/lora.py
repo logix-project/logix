6956531a279055a85c744060d6412594514e5314
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-11-12 17:19:34 -0500

    clean up code

--- analog/lora/lora.py
+++ analog/lora/lora.py
@@ -1,15 +1,22 @@
 import torch.nn as nn
+
 from analog.lora.modules import LoraLinear
+from analog.hessian import HessianHandlerBase
+
 
 class LoRAHandler:
     """
     Transforms a model into a Lora model.
     """
 
-    def __init__(self, config):
+    _SUPPORTED_MODULES = {nn.Linear, nn.Conv1d, nn.Conv2d}
+
+    def __init__(self, config, hessian_handler: HessianHandlerBase):
+        self.hessian_handler = hessian_handler
+
         self.config = config
         self.parse_config()
-        
+
     def parse_config(self):
         self.type = self.config.get("type", "random")
         self.rank = self.config.get("rank", 64)
@@ -18,30 +25,29 @@ class LoRAHandler:
         """
         Add LoRA modules to a model.
         """
-        if type_filter is None or nn.Linear in type_filter:
-            self.add_lora_linear(model, name_filter)
-
-        if type_filter is None or nn.Conv2d in type_filter:
-            self.add_lora_conv2d(model, name_filter)
-
-    def add_lora_linear(self, model, name_filter) -> None:
-        """
-        replace linear layers that match the name_filter with LoraLinear
-        """
         device = next(model.parameters()).device
-        modules_to_replace = []
-
         for name, module in model.named_modules():
-            name_not_filtered = name_filter is None or any(keyword in name for keyword in name_filter)
-            if isinstance(module, nn.Linear) and name_not_filtered:
-                modules_to_replace.append((name, module))
+            if len(list(module.children())) > 0:
+                continue
+            if not any(
+                isinstance(module, module_type)
+                for module_type in self._SUPPORTED_MODULES
+            ):
+                continue
+            if type_filter is not None and not any(
+                isinstance(module, module_type) for module_type in type_filter
+            ):
+                continue
+            if name_filter is not None and not any(
+                keyword in name for keyword in name_filter
+            ):
+                continue
 
-        for name, module in modules_to_replace:
-            new_module = LoraLinear(self.rank, module).to(device)
-            setattr(model, name, new_module)
-
-    def add_lora_conv2d(self, name, name_filter) -> None:
-        """
-        replace all conv layers that match the name_filter with LoraConv
-        """
-        pass
+            # Add Lora to filtered modules
+            if isinstance(module, nn.Linear):
+                new_module = LoraLinear(self.rank, module).to(device)
+                setattr(model, name, new_module)
+            elif isinstance(module, nn.Conv1d):
+                raise NotImplementedError
+            elif isinstance(module, nn.Conv2d):
+                raise NotImplementedError
