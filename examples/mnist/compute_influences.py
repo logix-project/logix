576f3b9284cfb36e2b20d1f7e643be9428a8f0a3
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-11-21 10:17:20 -0500

    clean up example code

--- examples/mnist/compute_influences.py
+++ examples/mnist/compute_influences.py
@@ -1,5 +1,10 @@
 import time
+import argparse
+
 import torch
+from analog import AnaLog
+from analog.utils import DataIDGenerator
+from analog.analysis import InfluenceFunction
 
 from train import (
     get_mnist_dataloader,
@@ -7,78 +12,66 @@ from train import (
     construct_mlp,
 )
 
+parser = argparse.ArgumentParser("MNIST Influence Analysis")
+parser.add_argument("--data", type=str, default="mnist", help="mnist or fmnist")
+parser.add_argument("--eval-idxs", type=int, nargs="+", default=[0])
+parser.add_argument("--damping", type=float, default=1e-5)
+args = parser.parse_args()
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DAMPING = 0.01
-LISSA_ITER = 100000
 
+model = construct_mlp().to(DEVICE)
+model.load_state_dict(
+    torch.load(f"checkpoints/{args.data}_0_epoch_9.pt", map_location="cpu")
+)
+model.eval()
 
-def single_checkpoint_influence(data_name="mnist", eval_idxs=(0,)):
-    model = construct_mlp().to(DEVICE)
+dataloader_fn = get_mnist_dataloader if args.data == "mnist" else get_fmnist_dataloader
+train_loader = dataloader_fn(
+    batch_size=512, split="train", shuffle=False, subsample=True
+)
+query_loader = dataloader_fn(
+    batch_size=1, split="valid", shuffle=False, indices=args.eval_idxs
+)
 
-    # Get a single checkpoint (first model_id and last epoch).
-    model.load_state_dict(
-        torch.load(f"checkpoints/{data_name}_0_epoch_9.pt", map_location="cpu")
-    )
-    model.eval()
+analog = AnaLog(project="test")
 
-    dataloader_fn = (
-        get_mnist_dataloader if data_name == "mnist" else get_fmnist_dataloader
-    )
-    train_loader = dataloader_fn(
-        batch_size=512, split="train", shuffle=False, subsample=True
-    )
-    query_loader = dataloader_fn(
-        batch_size=1, split="valid", shuffle=False, indices=eval_idxs
-    )
-
-    # Set-up
-    from analog import AnaLog
-    from analog.utils import DataIDGenerator
-
-    analog = AnaLog(project="test", config="config.yaml")
-
-    # Gradient & Hessian logging
-    analog.watch(model, name_filter=["1", "3", "5"])
-    id_gen = DataIDGenerator()
-    for inputs, targets in train_loader:
-        data_id = id_gen(inputs)
-        with analog(data_id=data_id, log=["grad"], save=True):
-            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
-            model.zero_grad()
-            outs = model(inputs)
-            loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
-            loss.backward()
-    analog.finalize()
-    print(analog.get_hessian_state())
-
-    # Influence Analysis
-    log_loader = analog.build_log_dataloader()
-
-    from analog.analysis import InfluenceFunction
-
-    analog.add_analysis({"influence": InfluenceFunction})
-    query_iter = iter(query_loader)
-    with analog(log=["grad"], test=True) as al:
-        test_input, test_target = next(query_iter)
-        test_input, test_target = test_input.to(DEVICE), test_target.to(DEVICE)
+# Gradient & Hessian logging
+analog.watch(model)
+id_gen = DataIDGenerator()
+for inputs, targets in train_loader:
+    data_id = id_gen(inputs)
+    with analog(data_id=data_id, log=["grad"], save=True):
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
         model.zero_grad()
-        test_out = model(test_input)
-        test_loss = torch.nn.functional.cross_entropy(
-            test_out, test_target, reduction="sum"
-        )
-        test_loss.backward()
-        test_log = al.get_log()
-    start = time.time()
-    if_scores = analog.influence.compute_influence_all(
-        test_log, log_loader, damping=1e-4
+        outs = model(inputs)
+        loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
+        loss.backward()
+analog.finalize()
+
+# Influence Analysis
+log_loader = analog.build_log_dataloader()
+
+analog.add_analysis({"influence": InfluenceFunction})
+query_iter = iter(query_loader)
+with analog(log=["grad"], test=True) as al:
+    test_input, test_target = next(query_iter)
+    test_input, test_target = test_input.to(DEVICE), test_target.to(DEVICE)
+    model.zero_grad()
+    test_out = model(test_input)
+    test_loss = torch.nn.functional.cross_entropy(
+        test_out, test_target, reduction="sum"
     )
+    test_loss.backward()
+    test_log = al.get_log()
+start = time.time()
+if_scores = analog.influence.compute_influence_all(
+    test_log, log_loader, damping=args.damping
+)
+_, top_influential_data = torch.topk(if_scores, k=10)
 
-    # Save
-    if_scores = if_scores.numpy().tolist()
-    torch.save(if_scores, "if_analog.pt")
-    print("Computation time:", time.time() - start)
-    print(sorted(if_scores)[:10], sorted(if_scores)[-10:])
-
-
-if __name__ == "__main__":
-    single_checkpoint_influence(data_name="mnist")
+# Save
+if_scores = if_scores.numpy().tolist()
+torch.save(if_scores, "if_analog.pt")
+print("Computation time:", time.time() - start)
+print("Top influential data indices:", top_influential_data.numpy().tolist())
--- examples/mnist/compute_influences_pca.py
+++ examples/mnist/compute_influences_pca.py
@@ -1,6 +1,10 @@
-import copy
 import time
+import argparse
+
 import torch
+from analog import AnaLog
+from analog.utils import DataIDGenerator
+from analog.analysis import InfluenceFunction
 
 from train import (
     get_mnist_dataloader,
@@ -8,84 +12,71 @@ from train import (
     construct_mlp,
 )
 
+parser = argparse.ArgumentParser("MNIST Influence Analysis")
+parser.add_argument("--data", type=str, default="mnist", help="mnist or fmnist")
+parser.add_argument("--eval-idxs", type=int, nargs="+", default=[0])
+parser.add_argument("--damping", type=float, default=1e-5)
+args = parser.parse_args()
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DAMPING = 0.01
-LISSA_ITER = 100000
 
+model = construct_mlp().to(DEVICE)
+model.load_state_dict(
+    torch.load(f"checkpoints/{args.data}_0_epoch_9.pt", map_location="cpu")
+)
+model.eval()
 
-def single_checkpoint_influence(data_name="mnist", eval_idxs=(0,)):
-    model = construct_mlp().to(DEVICE)
+dataloader_fn = get_mnist_dataloader if args.data == "mnist" else get_fmnist_dataloader
+train_loader = dataloader_fn(
+    batch_size=512, split="train", shuffle=False, subsample=True
+)
+query_loader = dataloader_fn(
+    batch_size=1, split="valid", shuffle=False, indices=args.eval_idxs
+)
 
-    # Get a single checkpoint (first model_id and last epoch).
-    model.load_state_dict(
-        torch.load(f"checkpoints/{data_name}_0_epoch_9.pt", map_location="cpu")
+analog = AnaLog(project="test")
+
+analog.watch(model)
+analog_kwargs = {"log": ["grad"], "hessian": True, "save": False}
+id_gen = DataIDGenerator()
+# Epoch 0: Compute Hessian
+# Epoch 1: Gradient logging w/ PCA+LoRA
+for epoch in range(2):
+    for inputs, targets in train_loader:
+        data_id = id_gen(inputs)
+        with analog(data_id=data_id, **analog_kwargs):
+            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+            model.zero_grad()
+            outs = model(inputs)
+            loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
+            loss.backward()
+    analog.finalize()
+    if epoch == 0:
+        analog_kwargs.update({"save": True})
+        analog.add_lora(model, parameter_sharing=False)
+
+log_loader = analog.build_log_dataloader()
+
+analog.add_analysis({"influence": InfluenceFunction})
+query_iter = iter(query_loader)
+with analog(log=["grad"], test=True) as al:
+    test_input, test_target = next(query_iter)
+    test_input, test_target = test_input.to(DEVICE), test_target.to(DEVICE)
+    model.zero_grad()
+    test_out = model(test_input)
+    test_loss = torch.nn.functional.cross_entropy(
+        test_out, test_target, reduction="sum"
     )
-    model.eval()
+    test_loss.backward()
+    test_log = al.get_log()
+start = time.time()
+if_scores = analog.influence.compute_influence_all(
+    test_log, log_loader, damping=args.damping
+)
+_, top_influential_data = torch.topk(if_scores, k=10)
 
-    dataloader_fn = (
-        get_mnist_dataloader if data_name == "mnist" else get_fmnist_dataloader
-    )
-    train_loader = dataloader_fn(
-        batch_size=512, split="train", shuffle=False, subsample=True
-    )
-    query_loader = dataloader_fn(
-        batch_size=1, split="valid", shuffle=False, indices=eval_idxs
-    )
-
-    # Set-up
-    from analog import AnaLog
-    from analog.utils import DataIDGenerator
-
-    analog = AnaLog(project="test", config="./examples/mnist/config.yaml")
-
-    analog.watch(model, name_filter=["1", "3", "5"])
-    analog_kwargs = {"log": ["grad"], "hessian": True, "save": False}
-    id_gen = DataIDGenerator()
-    # Epoch 0: Compute Hessian
-    # Epoch 1: Gradient logging w/ PCA+LoRA
-    for epoch in range(2):
-        for inputs, targets in train_loader:
-            data_id = id_gen(inputs)
-            with analog(data_id=data_id, **analog_kwargs):
-                inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
-                model.zero_grad()
-                outs = model(inputs)
-                loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
-                loss.backward()
-        analog.finalize()
-        if epoch == 0:
-            analog_kwargs.update({"save": True})
-            analog.add_lora(model, parameter_sharing=False)
-    print(analog.get_hessian_state())
-
-    log_loader = analog.build_log_dataloader()
-
-    # Influence Analysis
-    from analog.analysis import InfluenceFunction
-
-    analog.add_analysis({"influence": InfluenceFunction})
-    query_iter = iter(query_loader)
-    with analog(log=["grad"], test=True) as al:
-        test_input, test_target = next(query_iter)
-        test_input, test_target = test_input.to(DEVICE), test_target.to(DEVICE)
-        model.zero_grad()
-        test_out = model(test_input)
-        test_loss = torch.nn.functional.cross_entropy(
-            test_out, test_target, reduction="sum"
-        )
-        test_loss.backward()
-        test_log = al.get_log()
-    start = time.time()
-    if_scores = analog.influence.compute_influence_all(
-        test_log, log_loader, damping=1e-4
-    )
-
-    # Save
-    if_scores = if_scores.numpy().tolist()
-    torch.save(if_scores, "if_analog_lora64_pca.pt")
-    print("Computation time:", time.time() - start)
-    print(sorted(if_scores)[:10], sorted(if_scores)[-10:])
-
-
-if __name__ == "__main__":
-    single_checkpoint_influence(data_name="mnist")
+# Save
+if_scores = if_scores.numpy().tolist()
+torch.save(if_scores, "if_analog_lora64_pca.pt")
+print("Computation time:", time.time() - start)
+print("Top influential data indices:", top_influential_data.numpy().tolist())
