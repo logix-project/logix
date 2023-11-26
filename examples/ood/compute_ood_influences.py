e8e6f5e73688275e252790c5cfe4d30cf5e773ea
Youngseog Chung <youngsec@cs.cmu.edu>
2023-11-25 19:48:36 -0500

    finished mnist ood self influence example

--- examples/ood/compute_ood_influences.py
+++ examples/ood/compute_ood_influences.py
@@ -1,99 +1,133 @@
+import time
+import argparse
 
-import sys
-import time 
-
+import tqdm
+import numpy as np
 import torch
 
-from examples.mnist.utils import (
+from analog import AnaLog
+from analog.utils import DataIDGenerator
+from analog.analysis import InfluenceFunction
+from examples.mnist_influence.utils import (
     get_mnist_dataloader,
     get_fmnist_dataloader,
     construct_mlp,
 )
-from ood_utils import (
+from examples.ood.ood_utils import (
     get_ood_input_processor,
 )
 
+parser = argparse.ArgumentParser("OOD Self-influce Score Analysis")
+parser.add_argument("--id-data", type=str,default="mnist",
+                    help="mnist or fmnist; OOD is set to the other one")
+parser.add_argument("--damping", type=float, default=1e-5)
+args = parser.parse_args()
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DAMPING = 0.01
-LISSA_ITER = 100000
 
-def compute_ood_single_checkpoint_influence(source_data, target_model, checkpoint_dir, eval_idxs=(0,)):
-    model = construct_mlp().to(DEVICE)
+ood_data = "fmnist" if args.id_data == "mnist" else "mnist"
+model = construct_mlp().to(DEVICE)
 
-    # Get a single checkpoint (first model_id and last epoch).
-    model.load_state_dict(
-        torch.load(f"{checkpoint_dir}/checkpoints/{source_data}_0_epoch_9.pt", map_location="cpu")
-    )
-    model.eval()
+# Get a single checkpoint (first model_id and last epoch).
+model.load_state_dict(
+    torch.load(f"checkpoints/{args.id_data}_0_epoch_9.pt", map_location="cpu")
+)
+model.eval()
 
+id_dataloader_fn = (
+    get_mnist_dataloader if args.id_data == "mnist" else get_fmnist_dataloader
+)
+ood_dataloader_fn = (
+    get_mnist_dataloader if ood_data == "mnist" else get_fmnist_dataloader
+)
+id_train_loader = id_dataloader_fn(
+    batch_size=512, split="train", shuffle=False, subsample=True
+)
+id_query_loader = id_dataloader_fn(
+    batch_size=512,
+    split="valid",
+    shuffle=False,
+    subsample=True,
+)
+ood_query_loader = ood_dataloader_fn(
+    batch_size=512,
+    split="valid",
+    shuffle=False,
+    subsample=True,
+)
+ood_input_processor = get_ood_input_processor(
+    source_data=ood_data, target_model=args.id_data
+)
 
-    id_dataloader_fn = (
-        get_mnist_dataloader if source_data == "mnist" else get_fmnist_dataloader
-    )
-    ood_dataloader_fn = (
-        get_fmnist_dataloader if target_model == "mnist" else get_mnist_dataloader
-    )
-    id_train_loader = id_dataloader_fn(
-        batch_size=512, split="train", shuffle=False, subsample=True
-    )
-    # id_query_loader = id_dataloader_fn(
-    #     batch_size=1, split="valid", shuffle=False, indices=eval_idxs
-    # )
-    ood_query_loader = ood_dataloader_fn(
-        batch_size=1, split="valid", shuffle=False, indices=eval_idxs
-    )
-    ood_input_processor = get_ood_input_processor(source_data=source_data, target_model=target_model)
+# Set-up
+analog = AnaLog(project="test")
 
-    breakpoint()
-    # Set-up
-    from analog import AnaLog
-    from analog.utils import DataIDGenerator
-
-    analog = AnaLog(project="test", config="config.yaml")
-
-    # Gradient & Hessian logging
-    analog.watch(model, type_filter=[torch.nn.Linear], lora=False)
-    id_gen = DataIDGenerator()
-    for inputs, targets in id_train_loader:
-        data_id = id_gen(inputs)
-        with analog(data_id=data_id, log=["grad"], save=True):
-            inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
-            model.zero_grad()
-            outs = model(inputs)
-            loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
-            loss.backward()
-    analog.finalize(hessian_inverse=True, hessian_override=True)
-
-    breakpoint()
-    # Influence Analysis
-    log_loader = analog.build_log_dataloader()
-
-    from analog.analysis import InfluenceFunction
-
-    analog.add_analysis({"influence": InfluenceFunction})
-    query_iter = iter(ood_query_loader)
-    with analog(log=["grad"], test=True) as al:
-        test_input, test_target = next(query_iter)
-        test_input = ood_input_processor(test_input)
-        test_input, test_target = test_input.to(DEVICE), test_target.to(DEVICE)
+# Gradient & Hessian logging
+analog.watch(model, lora=False)
+id_gen = DataIDGenerator()
+for inputs, targets in id_train_loader:
+    data_id = id_gen(inputs)
+    with analog(data_id=data_id, log=["grad"], hessian=True, save=True):
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
         model.zero_grad()
-        test_out = model(test_input)
-        test_loss = torch.nn.functional.cross_entropy(
-            test_out, test_target, reduction="sum"
-        )
-        test_loss.backward()
-        test_log = al.get_log()
-    start = time.time()
-    if_scores = analog.influence.compute_influence_all(test_log, log_loader)
+        outs = model(inputs)
+        loss = torch.nn.functional.cross_entropy(outs, targets, reduction="sum")
+        loss.backward()
+analog.finalize()
 
-    # Save
-    if_scores = if_scores.numpy().tolist()
-    torch.save(if_scores, "if_analog.pt")
-    print("Computation time:", time.time() - start)
-    print(sorted(if_scores)[:10], sorted(if_scores)[-10:])
-    
-if __name__ == "__main__":
-    compute_ood_single_checkpoint_influence(source_data="mnist", target_model="fmnist", 
-                                            checkpoint_dir="/Users/youngsec/research/if/analog/examples/mnist",
-                                            eval_idxs=(0,))
-    # compute_ood_single_checkpoint_influence(source_data="fmnist", target_model="mnist", eval_idxs=(0,))
+# Influence Analysis
+analog.add_analysis({"influence": InfluenceFunction})
+
+print("Computing OOD self-influence scores...")
+ood_self_influence_scores = []
+for ood_test_input, ood_test_target in tqdm.tqdm(ood_query_loader):
+    with analog(log=["grad"], test=True) as al:
+        ood_test_input = ood_input_processor(ood_test_input)
+        ood_test_input, ood_test_target = (
+            ood_test_input.to(DEVICE),
+            ood_test_target.to(DEVICE),
+        )
+        model.zero_grad()
+        ood_test_out = model(ood_test_input)
+        ood_test_loss = torch.nn.functional.cross_entropy(
+            ood_test_out, ood_test_target, reduction="sum"
+        )
+        ood_test_loss.backward()
+        ood_test_log = al.get_log()
+    start = time.time()
+    if_scores = analog.influence.compute_self_influence(
+        ood_test_log, damping=args.damping
+    )
+    ood_self_influence_scores.append(if_scores.numpy().flatten())
+
+print("Computing ID self-influence scores...")
+id_self_influence_scores = []
+for id_test_input, id_test_target in tqdm.tqdm(id_query_loader):
+    with analog(log=["grad"], test=True) as al:
+        id_test_input, id_test_target = id_test_input.to(DEVICE), id_test_target.to(
+            DEVICE
+        )
+        model.zero_grad()
+        id_test_out = model(id_test_input)
+        id_test_loss = torch.nn.functional.cross_entropy(
+            id_test_out, id_test_target, reduction="sum"
+        )
+        id_test_loss.backward()
+        id_test_log = al.get_log()
+    start = time.time()
+    if_scores = analog.influence.compute_self_influence(
+        id_test_log, damping=args.damping
+    )
+    id_self_influence_scores.append(if_scores.numpy().flatten())
+
+# Save
+ood_sif_scores = np.concatenate(ood_self_influence_scores)
+id_sif_scores = np.concatenate(id_self_influence_scores)
+torch.save(ood_sif_scores, "ood_sif_scores.pt")
+torch.save(id_sif_scores, "id_sif_scores.pt")
+print(
+    f"OOD self-influence scores: mean={ood_sif_scores.mean():.2f}, std={ood_sif_scores.std():.2f}"
+)
+print(
+    f"ID self-influence scores: mean={id_sif_scores.mean():.2f}, std={id_sif_scores.std():.2f}"
+)
