fixes to _flush for hessian

analog/hessian/kfac.py
@@ -209,17 +209,23 @@ class KFACHessianHandler(HessianHandlerBase):
         return extract_backward_activations(data, module)
 
     def _flush(self):
-        if self.ekfac:
-            for module_name, ekfac_eigval in self.ekfac_eigval_state.items():
-                save_filename = os.path.join(
-                    self.log_dir, self.file_prefix + f"eigval_{module_name}.pt"
-                )
-                torch.save(ekfac_eigval.detach().cpu().numpy(), save_filename)
-        else:
-            for module_name, module_state in self.hessian_state.items():
-                for mode, covariance in module_state.items():
-                    save_filename = os.path.join(
-                        self.log_dir,
-                        self.file_prefix + f"covariance_{module_name}_{mode}.pt",
-                    )
-                    torch.save(covariance.detach().cpu().numpy(), save_filename)
+        if hasattr(self, "hessian_state"):
+            torch.save(
+                self.hessian_state,
+                os.path.join(self.log_dir, self.file_prefix + "hessian.pt"),
+            )
+        if hasattr(self, "hessian_eigvec_state"):
+            torch.save(
+                self.hessian_eigvec_state,
+                os.path.join(self.log_dir, self.file_prefix + "hessian_eigvec.pt"),
+            )
+        if hasattr(self, "hessian_eigval_state"):
+            torch.save(
+                self.hessian_eigval_state,
+                os.path.join(self.log_dir, self.file_prefix + "hessian_eigval.pt"),
+            )
+        if hasattr(self, "ekfac_eigval_state"):
+            torch.save(
+                self.ekfac_eigval_state,
+                os.path.join(self.log_dir, self.file_prefix + "ekfac_eigval.pt"),
+            )