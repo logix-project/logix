82baca42915fb23f3dd42a587042284a82939036
Sang Choe <sangkeuc@andrew.cmu.edu>
2023-12-09 10:41:59 -0500

    rewrite `RawHessianHandler`

--- analog/hessian/raw.py
+++ analog/hessian/raw.py
@@ -6,7 +6,7 @@ import torch.distributed as dist
 from einops import rearrange
 
 from analog.constants import FORWARD, BACKWARD
-from analog.utils import deep_get, get_world_size, nested_dict, get_logger
+from analog.utils import get_world_size, nested_dict, get_logger
 from analog.hessian.base import HessianHandlerBase
 from analog.hessian.utils import (
     extract_forward_activations,
@@ -22,40 +22,38 @@ class RawHessianHandler(HessianHandlerBase):
     def parse_config(self) -> None:
         self.log_dir = self.config.get("log_dir")
         self.damping = self.config.get("damping", 1e-2)
-        self.reduce = self.config.get("reduce", True)
 
     @torch.no_grad()
-    def on_exit(self, current_log=None) -> None:
-        if self.reduce:
-            for module_name, module_grad in current_log.items():
-                flat_grad = rearrange(module_grad, "b ... -> b (...)").cpu().detach()
-                grad_dim = flat_grad.shape[-1]
-                if module_name not in self.hessian_state:
-                    self.hessian_state[module_name] = torch.zeros(
-                        (grad_dim, grad_dim), device="cpu"
-                    )
-                    self.sample_counter[module_name] = 0
-                self.hessian_state[module_name].addmm_(flat_grad.t(), flat_grad)
-                self.sample_counter[module_name] += flat_grad.shape[0]
+    def on_exit(self, log_state=None) -> None:
+        for module_name, module_grad in log_state:
+            self.update_hessian(module_name, module_grad)
 
     @torch.no_grad()
-    def update_hessian(
-        self,
-        module: nn.Module,
-        module_name: str,
-        mode: str,
-        data: torch.Tensor,
-        mask: Optional[torch.Tensor] = None,
-    ) -> None:
-        if not self.reduce:
-            flat_grad = rearrange(data, "b ... -> b (...)").cpu().detach()
-            grad_dim = flat_grad.shape[-1]
+    def update_hessian(self, module_name: str, data: torch.Tensor) -> None:
+        hessian_state = self._state.hessian_state
+        sample_counter = self._state.sample_counter
 
-            # update covariance
-            if module_name not in self.hessian_state:
-                self.hessian_state[module_name] = torch.zeros(
-                    (grad_dim, grad_dim), device="cpu"
-                )
-                self.sample_counter[module_name] = 0
-            self.hessian_state[module_name].addmm_(flat_grad.t(), flat_grad)
-            self.sample_counter[module_name] += data.shape[0]
+        flat_grad = rearrange(data, "b ... -> b (...)")
+
+        # update covariance
+        if module_name not in hessian_state:
+            hessian_state[module_name] = torch.zeros(
+                (flat_grad.shape[-1], flat_grad.shape[-1]), device="cpu"
+            )
+            sample_counter[module_name] = 0
+
+        if flat_grad.is_cuda:
+            # By default, the hessian state is stored on the CPU. However,
+            # computing/updating the hessian state is on CPU is slow. Thus,
+            # we move the hessian state to the GPU if the activation is on
+            # the GPU, and then move it back to the CPU asynchrously.
+            hessian_state_gpu = hessian_state[module_name][mode].to(
+                device=activations.device
+            )
+            hessian_state_gpu.addmm_(flat_grad.t(), flat_grad)
+            hessian_state[module_name][mode] = hessian_state_gpu.to(
+                device="cpu", non_blocking=True
+            )
+        else:
+            hessian_state[module_name].addmm_(flat_grad.t(), flat_grad)
+        sample_counter[module_name] += data.shape[0]
