03c2ce663fc98f05c28d4b036d5993f27fc8e5e6
hage1005 <56232812+hage1005@users.noreply.github.com>
2023-12-21 13:37:49 -0500

    Add pandas dataframe format (#57)
    * add influence_scores pandas df
    
    * add getter
    
    * formatting
    
    * return additional self.data_id for analog.get_log()
    
    * add save function for influence_scores
    
    * minor fix

--- analog/analysis/influence_function.py
+++ analog/analysis/influence_function.py
@@ -1,12 +1,17 @@
+import pandas as pd
 import torch
-from einops import einsum, rearrange, reduce
 
+from einops import einsum, rearrange, reduce
 from analog.utils import get_logger
 from analog.analysis import AnalysisBase
 from analog.analysis.utils import reconstruct_grad, do_decompose, rescaled_dot_product
 
 
 class InfluenceFunction(AnalysisBase):
+    def __init__(self, config, state):
+        super().__init__(config, state)
+        self.influence_scores = pd.DataFrame()
+
     def parse_config(self):
         return
 
@@ -52,7 +57,9 @@ class InfluenceFunction(AnalysisBase):
         return preconditioned
 
     @torch.no_grad()
-    def compute_influence(self, src, tgt, preconditioned=False, damping=None):
+    def compute_influence(
+        self, src, tgt, src_ids=None, tgt_ids=None, preconditioned=False, damping=None
+    ):
         if not preconditioned:
             src = self.precondition(src, damping)
 
@@ -66,9 +73,31 @@ class InfluenceFunction(AnalysisBase):
                 src_log_expanded * tgt_log_expanded, "n m a b -> n m", "sum"
             )
             total_influence += module_influence
+
+        if src_ids is not None and tgt_ids is not None:
+            assert total_influence.shape[0] == len(src_ids)
+            assert total_influence.shape[1] == len(tgt_ids)
+            # Ensure src_ids and tgt_ids are in the DataFrame's index and columns, respectively
+            self.influence_scores = self.influence_scores.reindex(
+                index=src_ids, columns=tgt_ids
+            )
+
+            # Assign total_influence values to the corresponding locations in influence_scores
+            src_indices = [
+                self.influence_scores.index.get_loc(src_id) for src_id in src_ids
+            ]
+            tgt_indices = [
+                self.influence_scores.columns.get_loc(tgt_id) for tgt_id in tgt_ids
+            ]
+
+            self.influence_scores.iloc[
+                src_indices, tgt_indices
+            ] = total_influence.numpy()
+
         return total_influence
 
-    def compute_self_influence(self, src, damping=None):
+    def compute_self_influence(self, src_log, damping=None):
+        src_ids, src = src_log
         src_pc = self.precondition(src, damping)
         total_influence = 0.0
         for module_name in src_pc.keys():
@@ -77,9 +106,21 @@ class InfluenceFunction(AnalysisBase):
             total_influence += module_influence.squeeze()
         return total_influence
 
-    def compute_influence_all(self, src, loader, damping=None):
+    def compute_influence_all(self, src_log, loader, damping=None):
         if_scores = []
+        src_ids, src = src_log
         src = self.precondition(src, damping)
         for tgt_ids, tgt in loader:
-            if_scores.append(self.compute_influence(src, tgt, preconditioned=True))
+            if_scores.append(
+                self.compute_influence(
+                    src, tgt, src_ids=src_ids, tgt_ids=tgt_ids, preconditioned=True
+                )
+            )
         return torch.cat(if_scores, dim=-1)
+
+    def get_influence_scores(self):
+        return self.influence_scores
+
+    def save_influence_scores(self, filename):
+        self.influence_scores.to_csv(filename, index=True, header=True)
+        get_logger().info(f"Influence scores saved to {filename}")
